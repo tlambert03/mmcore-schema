26f01634fba8ddbd60bcd26c5d2f4c23769847f4
Talley Lambert <talley.lambert@gmail.com>
2025-05-07 09:15:14 -0400

    add readme

--- src/mmcore_schema/__init__.py
+++ src/mmcore_schema/__init__.py
@@ -1,10 +1,41 @@
-"""Schema for mmCoreAndDevices"""
+"""Schema for mmCoreAndDevices."""
 
 from importlib.metadata import PackageNotFoundError, version
+from typing import Literal
 
 try:
     __version__ = version("mmcore-schema")
 except PackageNotFoundError:
     __version__ = "uninstalled"
-__author__ = "Talley Lambert"
-__email__ = "talley.lambert@gmail.com"
+
+
+from ._schema import (
+    ConfigGroup,
+    Configuration,
+    CoreProperties,
+    Device,
+    DeviceProperty,
+    MMConfigFile,
+    PixelSizeConfiguration,
+    PropertySetting,
+)
+
+__all__ = [
+    "ConfigGroup",
+    "Configuration",
+    "CoreProperties",
+    "Device",
+    "DeviceProperty",
+    "MMConfigFile",
+    "PixelSizeConfiguration",
+    "PropertySetting",
+    "__version__",
+    "print_schema",
+]
+
+
+def print_schema() -> None:
+    """Print the schema for mmCoreAndDevices to stdout."""
+    import json
+
+    print(json.dumps(MMConfigFile.model_json_schema(), indent=2))
